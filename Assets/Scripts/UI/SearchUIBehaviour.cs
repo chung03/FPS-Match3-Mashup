60dd68c34ef170fe62ade7af3010a6603395d4a5
Colin Hung <colinhung03@gmail.com>
2019-04-23 22:06:39 -0400

    Fixing issue with lobby UI, adding search behaviour

---
+++ Assets/Scripts/UI/SearchUIBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using LobbyUtils;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+using System.Net;
+
+public class SearchUIBehaviour : MonoBehaviour
+{
+	private static readonly string LOBBY_SCENE = "Assets/Scenes/LobbyScene.unity";
+
+	[SerializeField]
+	private GameObject joinGameButton;
+
+	[SerializeField]
+	private GameObject IPInputField;
+
+	[SerializeField]
+	private GameObject clientObject;
+
+	private ClientLobbyComponent client;
+	private string ipString;
+
+	private void Start()
+	{
+		IPInputField.GetComponent<InputField>().onEndEdit.AddListener(OnSetIP);
+	}
+
+	public void Init(ClientLobbyComponent _client)
+	{
+		client = _client;
+	}
+	
+	public void OnJoinClick()
+	{
+		GameObject newClient = Instantiate(clientObject);
+		//newClient.GetComponent<ClientConnectionsComponent>().Init(false, IPAddress.Parse(ipString), 9000);
+		newClient.GetComponent<ClientConnectionsComponent>().Init(false);
+		DontDestroyOnLoad(newClient);
+
+
+		SceneManager.LoadScene(LOBBY_SCENE);
+	}
+
+	public void OnSetIP(string _ipString)
+	{
+		ipString = _ipString;
+	}
+}
